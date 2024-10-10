9edc69faeca57a02898bbd9cb569d134f5b4c3bc
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-10-10 21:16:23 +0200

    use thread safe random

--- DockerNamesGenerator/DockerNameGeneratorFactory.cs
+++ DockerNamesGenerator/DockerNameGeneratorFactory.cs
@@ -2,10 +2,20 @@
 
 public static class DockerNameGeneratorFactory
 {
-    public static DockerNameGenerator Create() => new(Random.Shared);
+    static readonly Random SeedGenerationRandom = new();
+
+    public static DockerNameGenerator Create() => new(new Random(GetRandomSeedThreadSafe()));
     public static DockerNameGenerator Create(int seed) => new(new Random(seed));
     public static DockerNameGenerator Create(Guid seed) => new(new Random(Hash(seed)));
     public static DockerNameGenerator Create(Random random) => new(random);
+
+    static int GetRandomSeedThreadSafe()
+    {
+        lock (SeedGenerationRandom)
+        {
+            return SeedGenerationRandom.Next();
+        }
+    }
 
     static int Hash(Guid guid)
     {
