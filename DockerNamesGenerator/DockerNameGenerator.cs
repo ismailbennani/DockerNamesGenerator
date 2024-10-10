824dd254d0f02998622ce1c2a06566a89315dc0d
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-10-10 19:24:48 +0200

    implement random generation

---
+++ DockerNamesGenerator/DockerNameGenerator.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Shamelessly ported from https://github.com/moby/moby/blob/39f7b2b6d0156811d9683c6cb0743118ae516a11/pkg/namesgenerator/names-generator.go#L16
+ */
+
+namespace DockerNamesGenerator;
+
+public static class DockerNameGenerator
+{
+    public static string GenerateDockerLikeName() => GenerateDockerLikeName(Random.Shared);
+    public static string GenerateDockerLikeName(int seed) => GenerateDockerLikeName(new Random(seed));
+    public static string GenerateDockerLikeName(object? seed) => GenerateDockerLikeName(new Random(seed?.GetHashCode() ?? 0));
+
+    public static string GenerateDockerLikeName(Random random)
+    {
+        string result;
+        do
+        {
+            int adjectivesIndex = random.Next(Constants.Adjectives.Count);
+            string adjective = Constants.Adjectives[adjectivesIndex];
+
+            int namesIndex = random.Next(Constants.Names.Count);
+            string name = Constants.Names[namesIndex];
+
+            result = $"{adjective}_{name}";
+        } while (result == "boring_wozniak"); /* Steve Wozniak is not boring */
+
+        return result;
+    }
+}
---
+++ DockerNamesGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Shamelessly ported from https://github.com/moby/moby/blob/39f7b2b6d0156811d9683c6cb0743118ae516a11/pkg/namesgenerator/names-generator.go#L16
+ */
+
+using CommandLine;
+using DockerNamesGenerator;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(
+        opt =>
+        {
+            if (opt.Seed == null)
+            {
+                Console.WriteLine(DockerNameGenerator.GenerateDockerLikeName());
+            }
+            else
+            {
+                Console.WriteLine(DockerNameGenerator.GenerateDockerLikeName(opt.Seed));
+            }
+        }
+    );
+
+
+class Options
+{
+    [Value(0, Required = false, HelpText = "Value that will be used as a seed for the random generation.")]
+    public string? Seed { get; set; } = null;
+}
